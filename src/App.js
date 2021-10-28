1b6b2c2d7f2c5d7d02568c9bc5031e257eff1faa
Afaaq <afaaqmajeed123@gmail.com>
2021-10-28 16:17:52 +0530

    added first react file

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import Navbar from './components/Navbar'
+import UpperCase from './components/UpperCase'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar title="MyApp" head="Home" />
+      <UpperCase heading="Enter your text here" />
+
+
+
+
+
+    </>
+
+
+  );
+}
+
+export default App;
+
+
---
+++ src/components/UpperCase.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+export default function UpperCase(props) {
+
+    const [text, setText] = useState('Enter text here');
+
+    console.log(text);
+    const clicked = () => {
+        let newvar = text.toUpperCase();
+
+        setText(newvar);
+    }
+    const change = (event) => {
+        setText(event.target.value);
+
+
+    }
+
+    return (
+        <>
+            <div className="container mt-4">
+                <div className="form-floating">
+                    <h4>{props.heading}</h4>
+                    <textarea className="form-control" onChange={change} value={text} placeholder="text here" id="textarea" > </textarea>
+
+
+                </div>
+
+                <button type="button" className="btn btn-primary mt-2" onClick={clicked}>Submit</button>
+            </div>
+
+
+            <div className="container">
+                <h4>Word count and length of characters are as:</h4>
+                <p>No. of characters with spaces included: {text.length}</p>
+                <p>Word count: {text.split(' ').length}</p>
+
+
+            </div>
+
+        </>
+
+    );
+}
+UpperCase.propTypes = {
+    heading: PropTypes.string
+
+}
+UpperCase.defaultProps = {
+    heading: 'default heading'
+
+};
